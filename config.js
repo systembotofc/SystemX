7ddf3c14babf628785c7d4e814ff84d2a6efd644
𝑻𝑰𝑻𝑼𝑳𝑨𝑹 𝑱𝑶𝑺𝑿 <benjaminexe2023@gmail.com>
2024-08-14 04:38:00 -1100

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
-import { watchFile, unwatchFile } from 'fs' 
-import chalk from 'chalk'
-import { fileURLToPath } from 'url'
-import fs from 'fs'
-import cheerio from 'cheerio'
-import fetch from 'node-fetch'
-import axios from 'axios'
-import moment from 'moment-timezone' 
+import { watchFile, unwatchFile } from 'fs';
+import chalk from 'chalk';
+import { fileURLToPath } from 'url';
+import fs from 'fs';
+import cheerio from 'cheerio';
+import fetch from 'node-fetch';
+import axios from 'axios';
+import moment from 'moment-timezone';
 
 //*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
 
@@ -15,55 +15,72 @@ global.owner = [
   ['51936732723'],
   ['51936732723'],
   ['51936732723']
-]
+];
 
 //*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
 
-global.mods = []
-global.prems = []
-   
-//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
-
-global.packname = ``
-global.author = '{\n "bot": {\n   "name": "𝑺𝒚𝒔𝒕𝒆𝒎𝑿",\n     "author": "𝑫𝒆𝒗𝒆𝒍𝒐𝒑𝒆𝒓𝑿",\n   "status_bot": "active"\n }\n}'
-global.wait = '𝑺𝒚𝒔𝒕𝒆𝒎𝑿🤖'
-global.botname = '𝑺𝒚𝒔𝒕𝒆𝒎𝑿🤖'
-global.textbot = `𝑫𝒆𝒗𝒆𝒍𝒐𝒑𝒆𝒓𝑿👑`
-global.listo = '𝑬𝒏𝒕𝒓𝒆𝒈𝒂 𝑬𝒙𝒊𝒕𝒐𝒔𝒂✅'
-global.namechannel = '【 𝐒𝐘𝐒𝐓𝐄𝐌𝐗 - 𝐃𝐄𝐕𝐄𝐋𝐎𝐏𝐄𝐑𝐗 】'
+global.mods = [];
+global.prems = [];
 
 //*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
 
-global.catalogo = fs.readFileSync('./storage/img/catalogo.png')
-global.miniurl = fs.readFileSync('./storage/img/miniurl.jpg')
+global.packname = '';
+global.author = '{\n "bot": {\n   "name": "𝑺𝒚𝒔𝒕𝒆𝒎𝑿",\n     "author": "𝑫𝒆𝒗𝒆𝒍𝒐𝒑𝒆𝒓𝑿",\n   "status_bot": "active"\n }\n}';
+global.wait = '𝑺𝒚𝒔𝒕𝒆𝒎𝑿🤖';
+global.botname = '𝑺𝒚𝒔𝒕𝒆𝒎𝑿🤖';
+global.textbot = '𝑫𝒆𝒗𝒆𝒍𝒐𝒑𝒆𝒓𝑿👑';
+global.listo = '𝑬𝒏𝒕𝒓𝒆𝒈𝒂 𝑬𝒙𝒊𝒕𝒐𝒔𝒂✅';
+global.namechannel = '【 𝐒𝐘𝐒𝐓𝐄𝐌𝐗 - 𝐃𝐄𝐕𝐄𝐋𝐎𝐏𝐄𝐑𝐗 】';
 
 //*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
 
-global.group = 'https://whatsapp.com/channel/0029VajUEsCB4hdNTg04zh1u'
-global.canal = 'https://whatsapp.com/channel/0029VajUEsCB4hdNTg04zh1u'
+global.catalogo = fs.readFileSync('./storage/img/catalogo.png');
+global.miniurl = fs.readFileSync('./storage/img/miniurl.jpg');
 
 //*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
 
-global.estilo = { key: {  fromMe: false, participant: `0@s.whatsapp.net`, ...(false ? { remoteJid: "5219992095479-1625305606@g.us" } : {}) }, message: { orderMessage: { itemCount : -999999, status: 1, surface : 1, message: botname, orderTitle: 'Bang', thumbnail: catalogo, sellerJid: '0@s.whatsapp.net'}}}
+global.group = 'https://whatsapp.com/channel/0029VajUEsCB4hdNTg04zh1u';
+global.canal = 'https://whatsapp.com/channel/0029VajUEsCB4hdNTg04zh1u';
 
 //*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
 
-global.cheerio = cheerio
-global.fs = fs
-global.fetch = fetch
-global.axios = axios
-global.moment = moment	
+global.estilo = {
+  key: { 
+    fromMe: false, 
+    participant: `0@s.whatsapp.net`, 
+    ...(false ? { remoteJid: "5219992095479-1625305606@g.us" } : {}) 
+  }, 
+  message: { 
+    orderMessage: { 
+      itemCount: -999999, 
+      status: 1, 
+      surface: 1, 
+      message: global.botname, 
+      orderTitle: 'Bang', 
+      thumbnail: global.catalogo, 
+      sellerJid: '0@s.whatsapp.net'
+    }
+  }
+};
 
 //*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
 
-global.multiplier = 69 
-global.maxwarn = '2' // máxima advertencias
+global.cheerio = cheerio;
+global.fs = fs;
+global.fetch = fetch;
+global.axios = axios;
+global.moment = moment;
 
 //*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
 
-let file = fileURLToPath(import.meta.url)
+global.multiplier = 69;
+global.maxwarn = '2'; // máxima advertencias
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+let file = fileURLToPath(import.meta.url);
 watchFile(file, () => {
-  unwatchFile(file)
-  console.log(chalk.redBright("Update 'config.js'"))
-  import(`${file}?update=${Date.now()}`)
-})
+  unwatchFile(file);
+  console.log(chalk.redBright("Update 'config.js'"));
+  import(`${file}?update=${Date.now()}`);
+});
