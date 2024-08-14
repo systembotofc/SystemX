9807eb4c697033834ed1e5b8a1afb5c440ab78c6
𝑻𝑰𝑻𝑼𝑳𝑨𝑹 𝑱𝑶𝑺𝑿 <benjaminexe2023@gmail.com>
2024-08-13 21:10:39 -1100

    Update group-tagall.js

--- plugins/group-tagall.js
+++ plugins/group-tagall.js
@@ -1,27 +1,21 @@
-const handler = async (m, {isOwner, isAdmin, conn, text, participants, args, command}) => {
+const handler = async (m, {isOwner, isAdmin, conn, text, participants, args, command, usedPrefix}) => {
+  if (usedPrefix == 'a' || usedPrefix == 'A') return;
   if (!(isAdmin || isOwner)) {
     global.dfail('admin', m, conn);
     throw false;
-    var sum = member.length;
-  } else {
-    var sum = 0;
-    const total = 0;
-    var member = 0;
   }
   const pesan = args.join` `;
-  const oi = `${pesan}`;
-  let teks = `*Sumi - Sakurasawa ✨*\n\n *Integrantes :  ${participants.length}* ${oi}\n\n┌──⭓ Ya revivan\n`;
+const oi = `${pesan}\n> 𝐓𝐎𝐓𝐀𝐋 𝐃𝐄 𝐔𝐒𝐔𝐀𝐑𝐈𝐎𝐒: ${participants.length}`;
+  let teks = `${oi}\n\n┏・ꜱʏꜱᴛᴇᴍx╮\n`;
   for (const mem of participants) {
-    teks += `🌼 @${mem.id.split('@')[0]}\n`;
+    teks += `┣・🍓 @${mem.id.split('@')[0]}\n`;
   }
-  teks += `└───────⭓
-
-© 2024 Starlights Team | All rights reserved`;
+  teks += `┗・ᴅᴇᴠᴇʟᴏᴘᴇʀx ʙᴇɴᴊᴀᴍɪɴ`;
   conn.sendMessage(m.chat, {text: teks, mentions: participants.map((a) => a.id)} );
 };
 handler.help = ['tagall <mesaje>', 'invocar <mesaje>'];
 handler.tags = ['group'];
-handler.command = /^(tagall|invocar|marcar|todos|invocación|ta)$/i;
+handler.command = /^(tagall|invocar|invocacion|todos|invocación|aviso)$/i;
 handler.admin = true;
 handler.group = true;
 export default handler;
